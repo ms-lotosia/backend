b5da4f9c556716eca80a2326017d002a4216708b
nijataghayev <nicat.agayev.536@gmail.com>
2025-11-10 21:18:08 +0400

    Initial project setup

---
+++ src/main/java/com/company/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.company.apigateway;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
