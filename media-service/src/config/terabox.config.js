30129fd780864909d81ef5109a06e17c6f878dd6
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-01-04 14:22:24 +0400

    Organize codebase: remove sensitive files, standardize configs to YAML, reorganize media service structure, update .gitignore

--- media-service/src/config/terabox.config.js
+++ media-service/src/config/terabox.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-  credentials: {
-    ndus: 'Y2b2zg3teHuiPn6Y_IIAbQzHJjbwUYHb18QZdybQ',
-    appId: '250528',
-    uploadId: 'N1-MzcuMTE0LjE0Ny4yMDoxNzY1OTk3NjQ2OjczMDM0NjU5NjAxNzA4MjU2',
-    jsToken: '8f86106a',
-    browserId: 'KYcKCXVvqMhXJZ6H5lVUCz5EcjjOWfq-jSWtduEUJ7uUDxPSUxFhnLD2eJg='
-  },
-  uploadDir: '/uploads'
-};
+require('dotenv').config();
 
+module.exports = {
+    credentials: {
+        ndus: process.env.TERABOX_NDUS,
+        appId: process.env.TERABOX_APP_ID,
+        uploadId: process.env.TERABOX_UPLOAD_ID,
+        jsToken: process.env.TERABOX_JS_TOKEN,
+        browserId: process.env.TERABOX_BROWSER_ID
+    },
+    uploadDir: process.env.UPLOAD_DIR || '/uploads'
+};
