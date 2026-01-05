839f88011ea2fea8bfb6c9d0f9bd400470103b2c
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-01-06 02:13:31 +0400

    Revert "Revert password encoder back to BCryptPasswordEncoder"
    This reverts commit d9f5fc0d22dd4b9bc8af2220ef162606215ec162.

--- identity-service/src/main/java/com/lotosia/identityservice/config/PasswordEncoderConfig.java
+++ identity-service/src/main/java/com/lotosia/identityservice/config/PasswordEncoderConfig.java
@@ -3,14 +3,12 @@ package com.lotosia.identityservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Temporarily use NoOpPasswordEncoder for testing
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
