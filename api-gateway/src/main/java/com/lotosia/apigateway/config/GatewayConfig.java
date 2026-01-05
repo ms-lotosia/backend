28edd07d6bca07b8c50242b7e276319506bdd1bf
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-01-06 02:21:54 +0400

    Fix API Gateway circuit breaker dependencies - use spring-cloud-starter-circuitbreaker-resilience4j

--- api-gateway/src/main/java/com/lotosia/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/lotosia/apigateway/config/GatewayConfig.java
@@ -1,31 +1,13 @@
 package com.lotosia.apigateway.config;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-
 @Configuration
 public class GatewayConfig {
-
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                .circuitBreakerConfig(CircuitBreakerRegistry.ofDefaults().getConfiguration("default").orElseThrow())
-                .timeLimiterConfig(TimeLimiterRegistry.ofDefaults().getConfiguration("default").orElseThrow())
-                .build());
-    }
 
     @Bean
     public KeyResolver keyResolver() {
@@ -34,6 +16,6 @@ public class GatewayConfig {
 
     @Bean
     public RedisRateLimiter redisRateLimiter() {
-        return new RedisRateLimiter(10, 20, 1); 
+        return new RedisRateLimiter(10, 20, 1);
     }
 }
