50e1884e5563e4800782516b0e328cb63dbb7cc7
nijataghayev <nicat.agayev.536@gmail.com>
2026-01-15 22:27:39 +0400

    .

--- identity-service/src/main/java/com/lotosia/identityservice/util/ControllerUtils.java
+++ identity-service/src/main/java/com/lotosia/identityservice/util/ControllerUtils.java
@@ -1,5 +1,6 @@
 package com.lotosia.identityservice.util;
 
+import com.lotosia.identityservice.security.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
